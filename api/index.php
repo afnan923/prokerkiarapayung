85e2e15337b9f88ee45708538dc535c6d1c453e4
afnan923 <susumurni013@gmail.com>
2026-09-20 00:03:37 +0700

    fix vercel deployment config

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Set storage path to /tmp to prevent read-only filesystem error on Vercel
+$app->useStoragePath('/tmp');
+
+$app->handleRequest(Illuminate\Http\Request::capture());
