31af484de3c63b787717a96b0a84f13a8d2f6218
afnan923 <susumurni013@gmail.com>
2026-09-19 23:39:09 +0700

    fix laravel vercel deployment

--- api/index.php
+++ api/index.php
@@ -1,13 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
-
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$request = Request::capture();
-
-$response = $app->handleRequest($request);
-
-$response->send();
+$app->handleRequest(
+    Illuminate\Http\Request::capture()
+);
